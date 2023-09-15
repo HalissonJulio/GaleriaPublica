934ac63f1ef23fd893f7f3227eb913fc6b0aa067
Halisson Julio Lopes Pereira <halissonjulio@gmail.com>
2023-09-15 10:34:50 -0300

    Passo 8.4, 15/09/23

--- app/src/main/java/pereira/lopes/julio/halisson/galeirapublica/MainViewModel.java
+++ app/src/main/java/pereira/lopes/julio/halisson/galeirapublica/MainViewModel.java
@@ -5,6 +5,20 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
 public class MainViewModel extends AndroidViewModel {
+    LiveData<PagingData<ImageData>> pageLv;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        GalleryRepository galleryRepository = new GalleryRepository(application);
+        GalleryPagingSource galleryPagingSource = new GalleryPagingSource(galleryRepository);
+        Pager<Integer, ImageData> pager = new Pager(new PagingConfig(10), () -> galleryPagingSource);
+        CoroutineScope viewModelScope = ViewModelKt.getViewModelScope(this);
+        pageLv = PagingLiveData.cachedIn(PagingLiveData.getLiveData(pager), viewModelScope);
+    }
+
+    public LiveData<PagingData<ImageData>> getPageLv() {
+        return pageLv;
+    }
 
     int navigationOpSelected = R.id.gridViewOp;
 
